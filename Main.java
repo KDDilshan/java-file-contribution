edfcaabb087f705ad271e335eb64106083b9ccc4
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-06-28 15:34:18 +0530

    Some changes

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
-    public static void main(String[] args)
-     {
-        System.out.println("Hello world");
-    }
+  public static void main(String[] args) {
+    int a = 5;
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+  }
 }
